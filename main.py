77eae202f74280cf2287f4ac2dcdc7dab0b59bd2
Dougie Richardson <me@dougiewougie.com>
2024-08-04 15:55:11 +0100

    Authentication - added users

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from web import example
+from web import example, user
 
 app = FastAPI()
 
 # Routers are defined in the web folder
 app.include_router(example.router)
+app.include_router(user.router)
 
 if __name__ == "__main__":
     """Want a web server to be started when called direct"""
